426f9f0ba829af9c06878485cb23309da75366dd
aravindhsekarit <aravindh.sekarit@gmail.com>
2025-05-27 22:19:16 +0800

    test

--- main.tf
+++ main.tf
@@ -1,1 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "testaws" {
+  name        = "testaws"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (us-east-1)
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.testaws.name]
+
+  tags = {
+    Name = "TerraformEC2"
+  }
+}
+
+output "instance_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.example.public_ip
+}
+
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
 
